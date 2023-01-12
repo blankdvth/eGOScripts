fbafe4964ad47b71751773d3be7e9d5028cf4d74
Skle <vimalvenky24@gmail.com>
2023-01-11 22:55:33 -0600

    Add support for applications

--- EGO Forum Enhancement.user.js
+++ EGO Forum Enhancement.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EdgeGamers Forum Enhancement
 // @namespace    https://github.com/blankdvth/eGOScripts/blob/master/EGO%20Forum%20Enhancement.user.js
-// @version      3.0.1
+// @version      3.1.0
 // @description  Add various enhancements & QOL additions to the EdgeGamers Forums that are beneficial for Leadership members.
 // @author       blank_dvth, Skle, MSWS
 // @match        https://www.edgegamers.com/*
@@ -13,6 +13,32 @@
 
 'use strict';
 const MAUL_BUTTON_TEXT = "MAUL";
+
+/**
+ * Creates a preset button
+ * @param {string} text Button text
+ * @param {function(HTMLElementEventMap)} callback Function to call on click  
+ * @returns {HTMLButtonElement} Button
+ */
+function createPresetButton(text, callback) {
+    var button = document.createElement("span");
+    button.classList.add("button");
+    button.innerHTML = text;
+    button.onclick = callback;
+    button.style.marginLeft = "4px";
+    button.style.marginTop = "4px";
+    return button
+}
+
+/**
+ * Adds a preset button to the div
+ * @param {string} name Name of button
+ * @param {HTMLDivElement} div Div to add to
+ * @param {function(HTMLElementEventMap)} func Function to call on click
+ */
+function addPreset(name, div, func) {
+    div.appendChild(createPresetButton(name, func));
+}
 
 /**
  * Creates a button and adds it to the given div
@@ -289,6 +315,71 @@ function handleBanReport() {
 }
 
 /**
+ * Adds "On Hold" templates to the menu and increases the size of the explain box.
+ * @param {HTMLElementEventMap} event 
+ * @returns void
+ */
+function handleOnHold(event) {
+    if(event.target.nodeName != 'DIV' || !event.target.classList.contains('overlay-container'))
+        return;
+
+    // Event may fire twice - add a mark the first time it fires, and ignore the rest
+    var mark = document.createElement('input');
+    mark.type = 'hidden';
+    event.target.append(mark);
+    if(event.target.childNodes.length > 2)
+        return;
+
+    var body = event.target.querySelector('.overlay > .overlay-content > form > .block-container > .block-body');
+    var reason = body.querySelector(':nth-child(1) > dd > input');
+    var explain = body.querySelector(':nth-child(2) > dd > input');
+    // Convert the explain input into a textarea
+    explain.outerHTML = explain.outerHTML.replace('input', 'textarea');
+    // Variable gets dereferenced - reference it again
+    explain = body.querySelector(':nth-child(2) > dd > textarea');
+    explain.style.height = '200px';
+    explain.setAttribute('maxlength', '1024');
+    var div = body.querySelector(':nth-child(4) > dd > div > .formSubmitRow-controls');
+
+    addPreset("No MAUL account", div, function() {
+        reason.value = "MAUL account must be created and verified";
+        explain.value = "In order for you to fix this you'll need to click the MAUL link at the top of the page in the navbar, click \"Edit Game IDs,\" \
+then click the Sign in through Steam button under the Source ID section. Once you've done so, please reply to this post!"
+    })
+
+    addPreset("Steam Verification", div, function() {
+        reason.value = "Steam account must be verified in MAUL";
+        explain.value = "In order for you to fix this you'll need to click the MAUL link at the top of the page in the navbar, click \"Edit Game IDs,\" \
+then click the Sign in through Steam button under the Source ID section. Once you've done so, please reply to this post!"
+    })
+
+    addPreset("Minecraft Verification", div, function() {
+        reason.value = "Minecraft ID must be verified in MAUL";
+        explain.value = "In order for you to fix this you'll need to click the MAUL link at the top of the page in the navbar, click \"Edit Game IDs,\" \
+then under ID for Minecraft, input your Minecraft username, click Convert to Game ID, then log onto our Minecraft server. Once you've done so, please reply to this post!"
+    })
+
+    addPreset("Battlefield Verification", div, function() {
+        reason.value = "Battlefield account must be verified in MAUL";
+        explain.value = "In order for you to fix this you'll need to click the MAUL link at the top of the page in the navbar, in MAUL hover over the home link in the top left, \
+click help, then follow the instructions for Battlefield. Once you have done so, please reply to this post!";
+    })
+
+    addPreset("Discord Verification", div, function() {
+        reason.value = "Discord ID must be verfied in MAUL";
+        explain.value = "In order for you to fix this you'll need to click the MAUL link at the top of the page in the navbar, click \"Edit Game IDs,\" \
+then click the sign in through Discord button under the discord ID section. Once you have done so, please reply to this post!"
+    })
+
+    addPreset("Inappropriate Name", div, function() {
+        reason.value = "Inappropriate Name";
+        explain.value = "As for your name, Please click [URL='https://www.edgegamers.com/account/username']here[/URL] and fill out a name change request. \
+After you fill it out, please wait while your name change request is finalized and the change is completed. \
+Once it is done your application process will resume. If you want to have an understanding on our naming policy inside of eGO please click [URL='https://www.edgegamers.com/threads/378540/']here[/URL].";
+    });
+}
+
+/**
  * Adds Confidential banners on top and bottom of page
  */
 function handleLeadership() {
@@ -296,11 +387,21 @@ function handleLeadership() {
     generateRedText("80%");
 }
 
+/**
+ * Changes the target of the application links to open in a new tab
+ * @returns void
+ */
+function handleApplicationPage() {
+    document.querySelector('.dataList-row > :nth-child(2)').childNodes[1].setAttribute('target', '_blank');
+    document.querySelector('.dataList-row > :nth-child(2)').childNodes[3].setAttribute('target', '_blank');
+}
+
 (function () {
     // Determine what page we're on
     var url = window.location.href;
 
     document.body.addEventListener('DOMNodeInserted', tooltipMAULListener, false);
+    document.body.addEventListener('DOMNodeInserted', handleOnHold, false);
 
     // Add Helpful Links to the Navigation Bar
     var nav_list = document.querySelector(".p-nav-list");
@@ -317,6 +418,9 @@ function handleLeadership() {
 
     if (url.match(/^https:\/\/www\.edgegamers\.com\/forums\/?$/))  // Forums List
         handleForumsList();
+
+    if (url.match(/^https:\/\/www\.edgegamers\.com\/application\/\d+\/?$/))  // Application Page
+        handleApplicationPage();
 
     handleGenericThread();
 })();
