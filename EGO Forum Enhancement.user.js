d2fb1301bebb0e31c0160172bb9a3ef0319a39dc
Skle <vimalvenky24@gmail.com>
2023-01-11 23:23:14 -0600

    Bump version number

--- EGO Forum Enhancement.user.js
+++ EGO Forum Enhancement.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EdgeGamers Forum Enhancement
 // @namespace    https://github.com/blankdvth/eGOScripts/blob/master/EGO%20Forum%20Enhancement.user.js
-// @version      3.1.0
+// @version      3.1.1
 // @description  Add various enhancements & QOL additions to the EdgeGamers Forums that are beneficial for Leadership members.
 // @author       blank_dvth, Skle, MSWS
 // @match        https://www.edgegamers.com/*
