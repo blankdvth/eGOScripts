08e2421d4c479db883d42e409db18461d79ad340
blankdvth <contact@blankdvth.com>
2023-02-07 20:26:31 -0500

    Add additional fix

--- GameME Space Render.user.js
+++ GameME Space Render.user.js
@@ -1,13 +1,17 @@
 // ==UserScript==
 // @name         GameME Space Render
 // @namespace    https://github.com/blankdvth/eGOScripts/blob/master/GameME%20Space%20Render.user.js
-// @version      1.0.0
-// @description  Renders additional whitespace between words in GameME Chat History
+// @version      1.0.1
+// @description  Renders additional whitespace between words in GameME Chat History (and fixes console spam)
 // @author       blank_dvth
-// @match        https://*.gameme.com/*chat*
+// @match        http*://*.gameme.com/*chat*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=edgegamers.com
 // @grant        none
 // ==/UserScript==
+
+// The Global Chat page on GameME calls these two functions non-stop when hovering over messages and spams console. I got annoyed...
+window.Tip = function () { };
+window.UnTip = function () { };
 
 (function () {
     "use strict";
