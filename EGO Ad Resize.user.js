71a10a64a1d075798dce5803c3d6280fb42f1ec2
blank_dvth <contact@blankdvth.com>
2023-02-09 14:09:21 -0500

    Update EGO Ad Resize.user.js

--- EGO Ad Resize.user.js
+++ EGO Ad Resize.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EdgeGamers Ad Resize
 // @namespace    https://github.com/blankdvth/eGOScripts/blob/master/EGO%20Ad%20Resize.user.js
-// @version      1.1.1
+// @version      1.1.2
 // @description  Removes whitespace left over from ads on the EdgeGamers website. This is to be used in combination with an adblocker (such as U-Block Origin).
 // @author       blank_dvth, Skle, MSWS
 // @match        https://www.edgegamers.com/*
@@ -13,10 +13,7 @@
 
 (function () {
     // Remove banners
-    var banners = [...document.querySelectorAll('div[style$="height:90px;"]')];
-    banners.forEach((banner) => {
-        banner.remove();
-    });
+    document.querySelectorAll('div[style$="height:90px;"]').forEach((banner) => banner.remove());
 
     // Remove sidebar
     var mainBody = document.querySelector("div .p-body-main--withSidebar");
