bf832ae9203f0ac6a778d6f75dbe05d975e96fe8
blankdvth <contact@blankdvth.com>
2023-03-15 18:59:36 -0400

    Remove parenthesis from URL regex

--- src/EGO MAUL Enhancement.ts
+++ src/EGO MAUL Enhancement.ts
@@ -529,7 +529,7 @@ function handleProfile() {
                 }
             )
             .replaceAll(
-                /https?:\/\/(www\.)?[-a-zA-Z0-9.]{1,256}\.[a-zA-Z0-9]{2,6}\b(\/[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/g,
+                /https?:\/\/(www\.)?[-a-zA-Z0-9.]{1,256}\.[a-zA-Z0-9]{2,6}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)/g,
                 '<a href="$&" target="_blank" rel="external">$&</a>'
             )
             .replaceAll(
@@ -627,7 +627,7 @@ function updateBanNoteURLs() {
         // Replace the text with a linkified version
         const replaced = unescapedInnerHTML
             .replaceAll(
-                /https?:\/\/(www\.)?[-a-zA-Z0-9.]{1,256}\.[a-zA-Z0-9]{2,6}\b(\/[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/g,
+                /https?:\/\/(www\.)?[-a-zA-Z0-9.]{1,256}\.[a-zA-Z0-9]{2,6}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)/g,
                 '<a href="$&" target="_blank" rel="external">$&</a>'
             )
             .replaceAll(
