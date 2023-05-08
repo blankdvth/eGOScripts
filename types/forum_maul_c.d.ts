e14324f4f1c41c462dfda12e957501e959dfdfb0
blankdvth <contact@blankdvth.com>
2023-05-08 13:17:12 -0400

    Add types

---
+++ types/forum_maul_c.d.ts
@@ -0,0 +1,7 @@
+interface AddBan_Data {
+    name?: string | undefined;
+    id: string;
+    threadId?: string | undefined;
+    reporter?: string | undefined; // Currently unused
+    game?: string | undefined; // Currently unused
+}
---
+++ types/lz-string.d.ts
@@ -0,0 +1,18 @@
+declare var LZString: LZString;
+
+interface LZString {
+    compressToBase64(input: string): string;
+    decompressFromBase64(input: string): string;
+
+    compressToUTF16(input: string): string;
+    decompressFromUTF16(compressed: string): string;
+
+    compressToUint8Array(uncompressed: string): Uint8Array;
+    decompressFromUint8Array(compressed: Uint8Array): string;
+
+    compressToEncodedURIComponent(input: string): string;
+    decompressFromEncodedURIComponent(compressed: string): string;
+
+    compress(input: string): string;
+    decompress(compressed: string): string;
+}
