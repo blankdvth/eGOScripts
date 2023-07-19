d0ea84905646bc675392fe408435e101c7aebcc3
blankdvth <contact@blankdvth.com>
2023-07-19 11:43:52 -0700

    Bump version number

--- src/EGO Forum Enhancement.ts
+++ src/EGO Forum Enhancement.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EdgeGamers Forum Enhancement%RELEASE_TYPE%
 // @namespace    https://github.com/blankdvth/eGOScripts/blob/master/src/EGO%20Forum%20Enhancement.ts
-// @version      4.11.2
+// @version      4.11.4
 // @description  Add various enhancements & QOL additions to the EdgeGamers Forums that are beneficial for Leadership members.
 // @author       blank_dvth, Skle, MSWS, PixeL
 // @match        https://www.edgegamers.com/*
